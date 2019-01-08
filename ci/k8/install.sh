bb6e108b52bf189417e6d4d0ec61b7fe413763a6
Ankit.vij <ankit.vij@ip-10-10-180-38.ap-southeast-1.compute.internal>
2019-01-08 14:35:12 +0800

    Install aws-authenticator

--- ci/k8/install.sh
+++ ci/k8/install.sh
@@ -12,3 +12,9 @@ cp ci/k8/config ${HOME}/.kube/config
 # Fill out missing params in kubectl config file
 kubectl config set clusters.eks_terraform-eks-cluster.certificate-authority-data "$KUBE_CLUSTER_CERTIFICATE"
 kubectl config set clusters.eks_terraform-eks-cluster.server "$KUBE_CLUSTER_SERVER"
+
+
+curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.11.5/2018-12-06/bin/linux/amd64/aws-iam-authenticator
+chmod +x ./aws-iam-authenticator
+cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$HOME/bin:$PATH
+echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
